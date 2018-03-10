8e7ba2f4d280166206e41ea6243a5767b43a0bc5
Rustam Irzaev <irzaevrustam@gmail.com>
2018-03-10 19:07:34 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppVeyorTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello from {typeof(Program)}!");
+        }
+    }
+}
